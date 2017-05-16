ddc35f0a3d5f5cbdf61e65a4640ef0fdfcf144b7
axle <axlevisu@gmail.com>
2017-05-16 16:32:11 +0530

    Cleaning up the code

--- main.py
+++ main.py
@@ -9,17 +9,11 @@ def arraypow(x,A):
 	return np.prod(x**(A.T),axis=-1)
 
 def ode(y,t,A,Ok):
-	# theta, X = np.array(y[:A.shape[0]]), np.array(y[A.shape[0]:])
 	theta,X = y[:A.shape[0]],y[A.shape[0]:]
-	# theta,X = y
-	# print y
 	MprojReaction = A.dot(X - arraypow(theta,A))
 	forward_rate = arraypow(theta,-1*A.dot(Ok*(Ok <0)))
 	backward_rate = arraypow(theta,-1*A.dot(Ok*(Ok >0)))
 	EProjReaction = Ok.dot(backward_rate*(arraypow(X,-Ok*(Ok <0))) -  forward_rate*arraypow(X,Ok*(Ok >0))) 
-	# print MprojReaction.tolist() + EProjReaction.tolist()
-	# return MprojReaction.tolist() + EProjReaction.tolist()
-	# print np.concatenate((MprojReaction,EProjReaction))
 	return np.concatenate((MprojReaction,EProjReaction)).tolist()
 
 # Give Model Here
@@ -30,9 +24,7 @@ X_init = [0.5,0.25,0.25]
 param_init = [1,1]
 # A = [[3,1,0,2],[0,2,3,1]]
 # O = [[1,1,0,0],[1,1,1,1]]
-# u = [1,1]
 # X_init = [0.3,0.2,0.1,0.4]
-# param_init = [1,1]
 
 # Number of timesteps
 ts = 10000
@@ -63,10 +55,8 @@ else:
 if X_init is None:
 	# This code is incomplete
 	# TODO: Add code here for an appropiate kernel element, use sympy for null space
-	# seed = np.random.normal(0,1)
 	B = np.linalg.pinv(O)
-	X =  B.dot(u)
-	# k = np.random.uniform(0,-np.min(X0))
+	X =  B.dot(u) # TODO: Add a vector from the nullspace and make sure X is positive
 else:
 	# Makes sure sum of X is 1
 	X = 1.0*X_init/sum(X_init)
@@ -84,9 +74,7 @@ print "initial theta and X:"
 print theta, X
 
 t = np.linspace(0, 100, ts)
-# y0 = theta.tolist() + X.tolist()
 y0 = np.concatenate((theta,X))
-# y0 = (theta,X)
 sol = odeint(ode, y0, t, args=(A,Ok))
 # Final theta and X
 y = sol[-1,:]
